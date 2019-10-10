99525bb45dc52e2b263230a042797d601fbe93d8
guest <guest@portart-6.iuthaguenau.lan>
2019-10-10 11:14:17 +0200

    Allo ?

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Homeee</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Homeee Allo?</a></li>
 
                     </ul>
 
