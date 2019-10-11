d55cc2747414860cbb841786f77ab4c9ebb1343f
guest <guest@portart-4.iuthaguenau.lan>
2019-10-11 11:17:02 +0200

    Test branche global

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">test allez</a>
+                    <a href="https://laravel.com/docs">test allez global</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
