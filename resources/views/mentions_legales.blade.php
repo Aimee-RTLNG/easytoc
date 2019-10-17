81b6c6a7959e36acff79b8695844fb9d11d51c88
guest <guest@portart-6.iuthaguenau.lan>
2019-10-17 16:55:02 +0200

    start contenu

--- resources/views/mentions_legales.blade.php
+++ resources/views/mentions_legales.blade.php
@@ -5,4 +5,25 @@
 
 @section('content')
     <h1>Mentions légales</h1>
+
+    <p>Conformément aux dispositions des articles 6-III et 19 de la Loi n° 2004-575 du 21 juin 2004 pour la Confiance dans l'économie numérique, dite L.C.E.N., nous portons à la connaissance des utilisateurs et visiteurs du site : www.generer-mentions-legales.com les informations suivantes :</p>
+
+<h2>1. Informations légales :</h2>
+https://www.easytoc.fr et son activité sont un service de EasyToC SARL, au capital de 140 000 €.
+<ul>
+    <li>Siège social : 30 Rue du Maire André Traband, 67500 Haguenau</li>
+    <li>Numéro d’immatriculation 821 257 094 au RCS de Strasbourg</li>
+    <li>N° de TVA : FR 06821257094</li>
+    <li>Directeur de publication : Aimée RITLENG</li>
+    <li>Contact de la publication : contact@editioneo.com ou 01 79 72 50 22</li>
+    <li>Hébergeur : OVH SAS - 2 rue Kellermann - 59100 ROUBAIX/France</li>
+</ul>
+
+<h2>2. Données Personelles :</h2>
+
+<p>Les utilisateurs du site <strong>www.easytoc.fr</strong> disposent d'un droit d'accès, de modification, de rectification et de suppression des données qui les concernent en application de l’article 34 de la loi n°78.17 du 6 janvier 1978, dite loi informatique et libertés. Les utilisateurs du site www.atiweb.fr peuvent exercer ce droit en nous envoyant un e-mail à info@easytoc.fr.</p>
+
+
+
+
 @endsection
