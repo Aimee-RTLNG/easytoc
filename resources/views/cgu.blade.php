95faac7c767447b8d225fc4aa64328932cb91149
Louise Matt <louise.matt67@gmail.com>
2019-11-01 22:10:36 +0100

    Page int titre + page aide controller

--- resources/views/cgu.blade.php
+++ resources/views/cgu.blade.php
@@ -3,5 +3,10 @@
 @section('titre') Condition d'Utilisations - EasyToC @endsection
 
 @section('content')
-    <h1>CGU</h1>
+<div class="container">
+    <div class="entete">
+        <h2 class="entete__title">Condition d'Utilisations</h2>
+        <div class="entete__under"></div>
+    </div>
+</div>
 @endsection
