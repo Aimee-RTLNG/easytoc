1137b6319b60588a6f692e11c82775ad73331664
guest <guest@portart-6.iuthaguenau.lan>
2019-11-05 12:23:04 +0000

    modifs pages

--- resources/views/mentions_legales.blade.php
+++ resources/views/mentions_legales.blade.php
@@ -9,7 +9,7 @@
     <p>Conformément aux dispositions des articles 6-III et 19 de la Loi n° 2004-575 du 21 juin 2004 pour la Confiance dans l'économie numérique, dite L.C.E.N., nous portons à la connaissance des utilisateurs et visiteurs du site : www.generer-mentions-legales.com les informations suivantes :</p>
 
 <h2>1. Informations légales :</h2>
-https://www.easytoc.fr et son activité sont un service de EasyToC SARL, au capital de 140 000 €.
+<strong>https://www.easytoc.fr</strong> et son activité sont un service de <strong>EasyToC SARL</strong>, au capital de <strong>140 000 €.</strong>
 <ul>
     <li>Siège social : 30 Rue du Maire André Traband, 67500 Haguenau</li>
     <li>Numéro d’immatriculation 821 257 094 au RCS de Strasbourg</li>
@@ -21,16 +21,16 @@ https://www.easytoc.fr et son activité sont un service de EasyToC SARL, au capi
 
 <h2>2. Données Personelles :</h2>
 
-<p>Les utilisateurs du site <strong>www.easytoc.fr</strong> disposent d'un droit d'accès, de modification, de rectification et de suppression des données qui les concernent en application de l’article 34 de la loi n°78.17 du 6 janvier 1978, dite loi informatique et libertés. Les utilisateurs du site <strong>www.easytoc.fr</strong> peuvent exercer ce droit en nous envoyant un e-mail à info@easytoc.fr.</p>
+<p>Les utilisateurs du site <strong>www.easytoc.fr</strong> disposent d'un droit d'accès, de modification, de rectification et de suppression des données qui les concernent en application de l’article 34 de la loi n°78.17 du 6 janvier 1978, dite loi informatique et libertés. Les utilisateurs du site <strong>www.easytoc.fr</strong> peuvent exercer ce droit en nous envoyant un e-mail à <strong>info@easytoc.fr.</strong></p>
 <p>Les données à caractère personnel des clients de EasyToC ne sont collectées, traitées, utilisées et stockées sans autre accord exprès préalable que dans la mesure où ceci est nécessaire à la conclusion et la bonne exécution du contrat ainsi que pour les besoins de la facturation. Vos données ne sont pas à la disposition du public ou de tiers (à l'exception toutefois de nos prestataires techniques lorsqu'ils interviennent sur nos machines, mais ils sont tenus aux mêmes obligations de confidentialité et de sécurité que nous).</p>
 
 <h2>3. Accessibilité :</h2>
 
-<p>Le site www.easytoc.fr est par principe accessible aux utilisateurs 24/24h, 7/7j, sauf interruption, programmée ou non, pour les besoins de sa maintenance ou en cas de force majeure. En cas d’impossibilité d’accès au service, www.easytoc.fr s’engage à faire son maximum afin de rétablir l’accès au service et s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de l’intervention.  N’étant soumis qu’à une obligation de moyen, www.easytoc.fr ne saurait être tenu pour responsable de tout dommage, quelle qu’en soit la nature, résultant d’une indisponibilité du service.</p>
+<p>Le site <strong>www.easytoc.fr</strong> est par principe accessible aux utilisateurs 24/24h, 7/7j, sauf interruption, programmée ou non, pour les besoins de sa maintenance ou en cas de force majeure. En cas d’impossibilité d’accès au service, <strong>www.easytoc.fr</strong> s’engage à faire son maximum afin de rétablir l’accès au service et s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de l’intervention.  N’étant soumis qu’à une obligation de moyen, <strong>www.easytoc.fr</strong> ne saurait être tenu pour responsable de tout dommage, quelle qu’en soit la nature, résultant d’une indisponibilité du service.</p>
 
 <h2>4. Propriété Intellectuelle :</h2>
 
 <p>EasyToC SARL est propriétaire exclusif de tous les droits de propriété intellectuelle ou détient les droits d’usage sur tous les éléments accessibles sur le site, tant sur la structure que sur les textes, images, graphismes, logo, icônes…
-Toute reproduction totale ou partielle du site www.easytoco.fr, représentation, modification, publication, adaptation totale ou partielle de l'un quelconque de ces éléments, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de EasyToC SARL, propriétaire du site à l'email : contact@easytoc.fr, à défaut elle sera considérée comme constitutive d’une contrefaçon et passible de poursuite conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+Toute reproduction totale ou partielle du site www.easytoco.fr, représentation, modification, publication, adaptation totale ou partielle de l'un quelconque de ces éléments, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de <strong>EasyToC SARL</strong>, propriétaire du site à l'email : <strong>contact@easytoc.fr</strong>, à défaut elle sera considérée comme constitutive d’une contrefaçon et passible de poursuite conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
 
 @endsection
