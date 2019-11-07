f9dd098bd9bd2278860665bdbcb0c692b7f90e0a
Louise Matt <louise.matt67@gmail.com>
2019-11-07 10:20:29 +0100

    Réorganisation scss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -96,7 +96,7 @@
                         <li class="nav-items"><a class="nav-link text-light" href="{{ route('mentions_legales') }}">{{ __('Mentions légales') }}</a></li>
                         <li class="nav-items"><a class="nav-link text-light" href="{{ route('cgu') }}">{{ __('Conditions générales d\'utilisation') }}</a></li>
                         <li class="nav-items"><a class="nav-link text-light" href="{{ route('aide') }}">{{ __('Aide') }}</a></li>
-                        <li class="nav-items"><a class="nav-link text-light" >Copiright 2019</a></li>
+                        <li class="nav-items"><a class="nav-link text-light" >Copyright 2019</a></li>
                     </ul>
                 </div>
             </div>
