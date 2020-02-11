e74f819bc2b32673ade048457fd97a386f57f5ac
Louise Matt <louise.matt67@gmail.com>
2020-02-11 11:45:21 +0100

    merge

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel\easytoc\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\IUT\LP-MI-server\ProjetApp\easytoc\easytoc\resources\js\app.js */"./resources/js/app.js");
 
 
 /***/ })
