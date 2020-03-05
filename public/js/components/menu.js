5a6392d15a3ffb697d836ec8149b1b926b9dfc4e
Aimee RITLENG <aimeeritleng@outlook.com>
2020-03-05 20:41:54 +0100

    Npm run watch

--- public/js/components/menu.js
+++ public/js/components/menu.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\xampp2\htdocs\laravel\easytoc\resources\js\components\menu.js */"./resources/js/components/menu.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel\easytoc\resources\js\components\menu.js */"./resources/js/components/menu.js");
 
 
 /***/ })
