6b20c4aa55bf2833b86891af1bf13fa425f3ebf1
Aimee RITLENG <aimeeritleng@outlook.com>
2020-03-09 11:26:58 +0100

    Icones longues à charger : contourner le pb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,8 +35,9 @@
         <link rel="icon" href="{{ URL::asset('favicon.ico') }}" type="image/ico">
 
         <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <!-- <link rel="dns-prefetch" href="//fonts.gstatic.com"> -->
         <link rel="stylesheet" href="{{asset(mix('css/app.css'))}}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- page specific style -->
         @yield('pagespecificstyles')
